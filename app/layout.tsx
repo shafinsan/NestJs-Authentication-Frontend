1ca787667642ad55cc16b4d119cae08aff8a8783
shafinsan <eliasjabershafin599@gmail.com>
2025-09-15 09:08:48 +0600

    Your commit message here

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
           <Header />
-          <main className="flex-grow container mx-auto p-4">
+          <main className="flex-grow container mx-auto p-4 bg-white text-black">
             {children}
           </main>
           <Footer />
